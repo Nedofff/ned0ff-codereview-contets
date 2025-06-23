02b068fa43ea1b16740f5a3896d2fe3c98b893ed
Eduard Novikov <edyard.novikov.d@gmail.com>
2025-06-23 21:30:15 +0700

    chore: удалить swcMinify и optimizeCss из конфигурации Next.js

--- next.config.ts
+++ next.config.ts
@@ -15,7 +15,6 @@ const nextConfig: NextConfig = {
   },
   compress: true,
   poweredByHeader: false,
-  swcMinify: true,
   compiler: {
     removeConsole: process.env.NODE_ENV === "production",
   },
@@ -86,7 +85,6 @@ const nextConfig: NextConfig = {
   },
   experimental: {
     optimizePackageImports: ["@/ui-kit", "@/core", "@/widgets"],
-    optimizeCss: true,
   },
 };
 
