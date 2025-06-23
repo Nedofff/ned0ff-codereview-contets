861f3e1d2435fc247245d40e5f4c4d05ede4c927
Eduard Novikov <edyard.novikov.d@gmail.com>
2025-06-23 21:34:32 +0700

    feat: рефакторинг компонента BackgroundGradient с добавлением поддержки no-SSR

--- src/widgets/main-layout/components/background-gradient/background-gradient.tsx
+++ src/widgets/main-layout/components/background-gradient/background-gradient.tsx
@@ -1,1 +1,36 @@
-"use client";import Image from "next/image";import bgGradient from "./bg-gradient.png";import { usePathname } from "next/navigation";import { cn } from "@/core/utils";import { routes } from "@/core/router";export function BackgroundGradient() {  const pathname = usePathname();  const isGetUp = [routes.home, routes.aboutUs].includes(pathname);  return (    <div      className={cn(        "absolute pointer-events-none left-0 w-full h-[674px] z-1  -top-[230px]",        {          "top-0": isGetUp,        }      )}    >      <Image        src={bgGradient}        alt="Background stars"        fill        className="object-cover h-full pointer-events-none"        priority        draggable={false}      />    </div>  );}
+"use client";
+import Image from "next/image";
+import bgGradient from "./bg-gradient.png";
+import { usePathname } from "next/navigation";
+import { cn } from "@/core/utils";
+import { routes } from "@/core/router";
+import { noSsrImport } from "@/core/no-ssr-import";
+
+const BackgroundGradientComponent = () => {
+  const pathname = usePathname();
+  const isGetUp = [routes.home, routes.aboutUs].includes(pathname);
+  return (
+    <div
+      className={cn(
+        "absolute pointer-events-none left-0 w-full h-[674px] z-1  -top-[230px]",
+        {
+          "top-0": isGetUp,
+        }
+      )}
+    >
+      <Image
+        src={bgGradient}
+        alt="Background stars"
+        fill
+        className="object-cover h-full pointer-events-none"
+        priority
+        draggable={false}
+      />
+    </div>
+  );
+};
+
+export const BackgroundGradient = noSsrImport(
+  BackgroundGradientComponent,
+  () => null
+);
